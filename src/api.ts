9c699a513e03938dcae29d3576ed4450c5db30b2
Luiz Fernando Justo <fernando.jjusto@gmail.com>
2021-01-07 21:45:34 -0300

    Add API

---
+++ src/api.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+import axios from 'axios';
+
+
+const API_URL = 'http://localhost:8080';
+
+export function fetchProducts(){
+    return axios(`${API_URL}/products`)
+}
